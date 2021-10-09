b9d4e804a88b33a6b33c7fbd8a9d74a5798543e0
Bishnu Nath <bishnu1324@gmail.com>
2021-10-09 10:14:24 +0600

    32-5 Dynamically Display Loaded Data On Your Website

--- js/api.js
+++ js/api.js
@@ -17,5 +17,10 @@ function loadPosts() {
 }
 
 function displayUsers(data) {
-  console.log(data);
+  const ul = document.getElementById('users');
+  for (const user of data) {
+    const li = document.createElement('li');
+    li.innerText = `Name: ${user.name}  Email: ${user.email}`;
+    ul.appendChild(li);
+  }
 }
